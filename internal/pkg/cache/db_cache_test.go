d2a1410406374c7c31a3c59291a0547e45514160
vaayne <liu.vaayne@gmail.com>
2024-07-20 16:58:54 +0800

    fix testcontainers using postgres

--- internal/pkg/cache/db_cache_test.go
+++ internal/pkg/cache/db_cache_test.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"path/filepath"
+	"strings"
 	"testing"
 	"time"
 	"vibrain/internal/pkg/db"
@@ -19,6 +20,8 @@ import (
 type CacheTestSuite struct {
 	suite.Suite
 	*DbCache
+	ctx       context.Context
+	container *postgres.PostgresContainer
 }
 
 type User struct {
@@ -29,6 +32,7 @@ type User struct {
 func (s *CacheTestSuite) SetupSuite() {
 	// Create a new context
 	ctx := context.Background()
+	s.ctx = ctx
 	// set up pg container
 	pool, err := s.initPostgres(ctx)
 	if err != nil {
@@ -36,6 +40,13 @@ func (s *CacheTestSuite) SetupSuite() {
 	}
 	// Create a new cache instance
 	s.DbCache = NewDBCache(pool)
+}
+
+func (s *CacheTestSuite) TearDownSuite() {
+	// Clean up the container
+	if err := s.container.Terminate(s.ctx); err != nil {
+		log.Fatalf("failed to terminate container: %s", err)
+	}
 }
 
 func (s *CacheTestSuite) initPostgres(ctx context.Context) (*db.Pool, error) {
@@ -58,15 +69,19 @@ func (s *CacheTestSuite) initPostgres(ctx context.Context) (*db.Pool, error) {
 		log.Fatalf("failed to start container: %s", err)
 	}
 
-	// Clean up the container
-	defer func() {
-		if err := postgresContainer.Terminate(ctx); err != nil {
-			log.Fatalf("failed to terminate container: %s", err)
-		}
-	}()
+	s.container = postgresContainer
 
 	// Create a new pool
-	pool, err := db.NewPool(ctx, fmt.Sprintf("user=%s password=%s dbname=%s sslmode=disable", dbUser, dbPassword, dbName))
+	ep, err := postgresContainer.Endpoint(ctx, "")
+	if err != nil {
+		return nil, err
+	}
+
+	eps := strings.Split(ep, ":")
+
+	databaseUrl := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable", eps[0], eps[1], dbUser, dbPassword, dbName)
+
+	pool, err := db.NewPool(ctx, databaseUrl)
 	if err != nil {
 		return nil, err
 	}
@@ -82,9 +97,9 @@ func (s *CacheTestSuite) TestCacheString() {
 	cacheKey := NewCacheKey(domain, key)
 	s.SetWithContext(ctx, cacheKey, value, 1*time.Hour)
 
-	val, ok := s.GetWithContext(ctx, cacheKey)
+	val, ok := Get[string](ctx, s.DbCache, cacheKey)
 	assert.True(s.T(), ok)
-	assert.Equal(s.T(), value, val.(string))
+	assert.Equal(s.T(), value, *val)
 }
 
 func (s *CacheTestSuite) TestCacheStructData() {
@@ -96,9 +111,9 @@ func (s *CacheTestSuite) TestCacheStructData() {
 	cacheKey := NewCacheKey(domain, key)
 	s.SetWithContext(ctx, cacheKey, value, 1*time.Hour)
 
-	val, ok := s.GetWithContext(ctx, cacheKey)
+	user, ok := Get[User](ctx, s.DbCache, cacheKey)
 	assert.True(s.T(), ok)
-	assert.Equal(s.T(), value, val.(User))
+	assert.Equal(s.T(), value, *user)
 }
 
 func TestCacheTestSuite(t *testing.T) {
