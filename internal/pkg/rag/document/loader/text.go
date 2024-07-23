82710b345a9c28ff0ead77e237d2d659cf2b0be0
vaayne <liu.vaayne@gmail.com>
2024-07-23 23:37:14 +0800

    add common rag libs

---
+++ internal/pkg/rag/document/loader/text.go
@@ -0,0 +1,39 @@
+package loader
+
+import (
+	"bytes"
+	"context"
+	"io"
+	"vibrain/internal/pkg/rag/document"
+	"vibrain/internal/pkg/rag/document/transformer"
+)
+
+// TextLoader loads text data from an io.Reader.
+type TextLoader struct {
+	io.Reader
+}
+
+// NewText creates a new text loader with an io.Reader.
+func NewTextLoader(r io.Reader) TextLoader {
+	return TextLoader{
+		Reader: r,
+	}
+}
+
+// Load reads from the io.Reader and returns documents with the data.
+func (r TextLoader) Load(ctx context.Context, transformers ...transformer.Transformer) ([]document.Document, error) {
+	buf := new(bytes.Buffer)
+	_, err := io.Copy(buf, r.Reader)
+	if err != nil {
+		return nil, err
+	}
+
+	docs := []document.Document{
+		{
+			Content:  buf.String(),
+			Metadata: map[string]any{},
+		},
+	}
+
+	return transformerPipeline(docs, transformers...)
+}
---
+++ internal/pkg/rag/document/transformer/splitter.go
@@ -0,0 +1,59 @@
+package transformer
+
+import (
+	"errors"
+	"vibrain/internal/pkg/rag/document"
+	"vibrain/internal/pkg/rag/textsplitter"
+)
+
+// ErrMismatchMetadatasAndText is returned when the number of texts and metadatas
+// given to CreateDocuments does not match. The function will not error if the
+// length of the metadatas slice is zero.
+var ErrMismatchMetadatasAndText = errors.New("number of texts and metadatas does not match")
+
+func WithTextSplitter(textSplitter textsplitter.TextSplitter) Transformer {
+	return func(docs []document.Document) ([]document.Document, error) {
+		texts := make([]string, 0)
+		metadatas := make([]map[string]any, 0)
+		for _, document := range docs {
+			texts = append(texts, document.Content)
+			metadatas = append(metadatas, document.Metadata)
+		}
+
+		return CreateDocuments(textSplitter, texts, metadatas)
+	}
+}
+
+func CreateDocuments(textSplitter textsplitter.TextSplitter, texts []string, metadatas []map[string]any) ([]document.Document, error) {
+	if len(metadatas) == 0 {
+		metadatas = make([]map[string]any, len(texts))
+	}
+
+	if len(texts) != len(metadatas) {
+		return nil, ErrMismatchMetadatasAndText
+	}
+
+	documents := make([]document.Document, 0)
+
+	for i := 0; i < len(texts); i++ {
+		chunks, err := textSplitter.Split(texts[i])
+		if err != nil {
+			return nil, err
+		}
+
+		for _, chunk := range chunks {
+			// Copy the document metadata
+			curMetadata := make(map[string]any, len(metadatas[i]))
+			for key, value := range metadatas[i] {
+				curMetadata[key] = value
+			}
+
+			documents = append(documents, document.Document{
+				Content:  chunk,
+				Metadata: curMetadata,
+			})
+		}
+	}
+
+	return documents, nil
+}
