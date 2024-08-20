a63dd89636dbb5dbba1a9a28d39b1792906bc95d
liu.vaayne <liu.vaayne@gmail.com>
2024-08-20 11:09:23 +0800

    refactor: Remove unused imports and update API configuration

--- web/src/libs/api.js
+++ web/src/libs/api.js
@@ -1,13 +1,10 @@
+import { QueryClient } from "@tanstack/react-query";
 import { AssistantsApi, AuthApi, Configuration, ToolsApi } from "../sdk/index";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 export const queryClient = new QueryClient();
 
 const config = new Configuration({
-  // apiKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3MjQxNDAwNTQsInVzZXJfaWQiOiJmZDM5YjM5Zi1hOTc1LTQyYzYtODdjMy1mNDFiMjdlZGY1NTcifQ.6o7N3RoBltVsfHaYo1rju4oFnpZ1J64zGEhbbSckjgc",
-  // accessToken:
-  //     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3MjQxNDAwNTQsInVzZXJfaWQiOiJmZDM5YjM5Zi1hOTc1LTQyYzYtODdjMy1mNDFiMjdlZGY1NTcifQ.6o7N3RoBltVsfHaYo1rju4oFnpZ1J64zGEhbbSckjgc",
-  credentials: "include",
+    credentials: "include",
 });
 
 export const assistantAPI = new AssistantsApi(config);
