bf48b48efa27789462d06c0fd23ea4ff068b1e5e
vaayne <liu.vaayne@gmail.com>
2024-09-03 08:57:11 +0800

    feat: support vite PWA

--- web/vite.config.js
+++ web/vite.config.js
@@ -2,10 +2,14 @@ import { resolve } from "path";
 import { defineConfig } from "vite";
 
 import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA()
+  ],
   // esbuild: {
   //   supported: {
   //     "top-level-await": true,
