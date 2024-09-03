d68e6c2de8cf40354b672f98b16c5ab3b5229c54
vaayne <liu.vaayne@gmail.com>
2024-09-03 14:42:36 +0800

    feat: stop working on PWA

--- web/vite.config.js
+++ web/vite.config.js
@@ -2,22 +2,21 @@ import { resolve } from "path";
 import { defineConfig } from "vite";
 
 import react from "@vitejs/plugin-react";
-import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    VitePWA({
-      workbox: {
-        maximumFileSizeToCacheInBytes: 10000000,
-      },
-      registerType: "autoUpdate",
-      devOptions: {
-        enabled: true,
-        type: "module",
-      },
-    }),
+    // VitePWA({
+    //   workbox: {
+    //     maximumFileSizeToCacheInBytes: 10000000,
+    //   },
+    //   registerType: "autoUpdate",
+    //   devOptions: {
+    //     enabled: true,
+    //     type: "module",
+    //   },
+    // }),
   ],
   // esbuild: {
   //   supported: {
