f9fa4f10d9012ee0290e6f0817f9c23b950c73c8
vaayne <liu.vaayne@gmail.com>
2024-12-30 22:41:42 +0800

    ✨ feat: Update bookmark fetcher to use JinaFetcher

--- internal/port/bots/handlers/websummary.go
+++ internal/port/bots/handlers/websummary.go
@@ -109,7 +109,7 @@ func (h *Handler) saveBookmark(ctx context.Context, tx db.DBTX, url string, user
 	result, err := h.queue.Insert(ctx, queue.CrawlerWorkerArgs{
 		ID:          bookmark.ID,
 		UserID:      bookmark.UserID,
-		FetcherName: bookmarks.HttpFetcher,
+		FetcherName: bookmarks.JinaFetcher,
 	}, nil)
 	if err != nil {
 		logger.FromContext(ctx).Error("failed to insert job", "err", err)
